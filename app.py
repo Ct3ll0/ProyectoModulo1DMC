daebc8e4e9b98bead4669451d9a37293fc1d6390
Ct3ll0 <117622563+Ct3ll0@users.noreply.github.com>
2026-07-09 22:20:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import streamlit as st
 
-st.image("Python_logo.png")
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
-valor_inicial=st.number_input("Ingrese el valor inicial",value=0)
-valor_final=st.number_input("Ingrese el valor final",value=1)
 
-lista_numerica=list(range(valor_inicial,valor_final))
+st.image("pythonlogo.png")
+st.sidebar.image("dmclogo.png")
 
-st.write(lista_numerica)
+modulo=st.sidebar.selectbox("Elija un módulo",["Módulo Listas","Módulo Array","Módulo funciones"]
+
+if modulo == "Módulo Listas":
+                            
+  valor_inicial=st.number_input("Ingrese el valor inicial",value=0)
+  valor_final=st.number_input("Ingrese el valor final",value=1)
+  
+  lista_numerica=list(range(valor_inicial,valor_final))
+  
+  st.write(lista_numerica)
+
+elif modulo == "Módulo Array":
+  st.write("Estás en el módulo de arreglos")
+
+else:
+  st.write("Estás en el módulo de funciones")
