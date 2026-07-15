24ae8abe181cb5166830957e6f7e6f5fd902dc7b
Ct3ll0 <117622563+Ct3ll0@users.noreply.github.com>
2026-07-14 22:24:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import libreria_funciones as lf
 
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
@@ -7,32 +8,37 @@ st.sidebar.title("Parámetros")
 st.image("pythonlogo.png")
 st.sidebar.image("dmclogo.png")
 
-modulo=st.sidebar.selectbox("Elija un módulo",["Módulo Listas","Módulo Array","Módulo funciones"])
+modulo = st.sidebar.selectbox("Elija un módulo", ["Módulo Listas","Módulo Array","Módulo funciones"])
 
 if modulo == "Módulo Listas":
-                            
-  valor_inicial=st.number_input("Ingrese el valor inicial",value=0)
-  valor_final=st.number_input("Ingrese el valor final",value=1)
-  
-  lista_numerica=list(range(valor_inicial,valor_final))
-  
+
+  valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
+  valor_final = st.number_input("Ingrese el valor final", value = 1)
+  lista_numerica = list(range(valor_inicial,valor_final))
   st.write(lista_numerica)
 
 elif modulo == "Módulo Array":
-  st.write("Estás en el módulo de arreglos")
+  st.write("Estas en el módulo de arreglos")
   
-  limite_inferior=st.number_input("Ingrese el límite inferior",value=1200)
-  limite_superior=st.number_input("Ingrese el límite superior",value=1250)
+  limite_inferior = st.number_input("Ingrese el límite inferior", value=1200)
+  limite_superior = st.number_input("Ingrese el límite superior", value = 1250)
   cantidad_datos =  st.number_input("Ingrese totalidad de datos a crear", value = 31)
-  
-  datos_produccion=np.random.randint(limite_inferior, limite_superior, cantidad_datos)
+
+  datos_produccion = np.random.randint(limite_inferior, limite_superior, cantidad_datos)
 
   st.write(datos_produccion)
 
-  st.write("La producción total es:" , np.sum(datos_produccion))
-  st.write("La producción promedio es:" , np.mean(datos_produccion))
-
-
+  st.write("La producción total es:" ,  np.sum(datos_produccion))
+  st.write("La producción promedio es:" , np.mean(datos_produccion) )
 
 else:
-  st.write("Estás en el módulo de funciones")
+  st.write("Estas en el módulo de funciones")
+  
+  principal = st.number_input("Ingrese el Monto del préstamo", value=0)
+  tasa_anual  = st.number_input("Ingrese Tasa anual en decimal", value=0.10)
+  anios = st.number_input("Ingrese el Número de años del préstamo", value=1)
+  pagos_por_anio =st.number_input("Ingrese el Cantidad de pagos por año", value=12)
+  
+  cuota  = lf.cuota_prestamo(principal, tasa_anual, anios, pagos_por_anio)
+  
+  st.write("La cuota mensual de pago será:", cuota)
